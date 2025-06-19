0163d06e01043a259cf6a53a75d1c7038f3d1bb0
aleksaelezovic <aleksaelezovic@gmail.com>
2025-06-19 20:05:51 +0200

    Delete example math package

--- typescript/apps/web/app/page.tsx
+++ typescript/apps/web/app/page.tsx
@@ -1,6 +1,5 @@
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
-import { add } from "@repo/math/add";
 import styles from "./page.module.css";
 
 type Props = Omit<ImageProps, "src"> & {
@@ -15,7 +14,6 @@ const ThemeImage = (props: Props) => {
     <>
       <Image {...rest} src={srcLight} className="imgLight" />
       <Image {...rest} src={srcDark} className="imgDark" />
-      <div>{add(1, 2)}</div>
     </>
   );
 };
