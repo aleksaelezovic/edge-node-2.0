b7ba0eafe0ed9bded56b779f5aa62bb1117d61fa
aleksaelezovic <aleksaelezovic@gmail.com>
2025-06-20 16:43:50 +0200

    Add code for registering plugins and server

--- typescript/packages/plugins/src/index.ts
+++ typescript/packages/plugins/src/index.ts
@@ -1,6 +1,32 @@
-import type { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { Hono } from "hono";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { registerMcp } from "./registerMcp";
 
 type DkgContext = any;
-type DkgPlugin = (ctx: DkgContext, mcp: McpServer) => void;
+type DkgPlugin = (ctx: DkgContext, mcp: McpServer, api: Hono) => void;
 
 export const defineDkgPlugin = (plugin: DkgPlugin) => plugin;
+
+export const createPluginApi = ({
+  name,
+  version,
+  plugins,
+  engineUrl,
+}: {
+  name: string;
+  version: string;
+  plugins: DkgPlugin[];
+  engineUrl: string;
+}) => {
+  const ctx = { engineUrl }; // TODO:
+  const api = new Hono();
+  registerMcp(api, () => {
+    const server = new McpServer({ name, version });
+    plugins.forEach((plugin) => plugin(ctx, server, new Hono()));
+    return server;
+  });
+  plugins.forEach((plugin) =>
+    plugin(ctx, new McpServer({ name, version }), api),
+  );
+  return api;
+};
---
+++ typescript/packages/plugins/src/registerMcp.ts
@@ -0,0 +1,63 @@
+import type { Context, Hono } from "hono";
+import { randomUUID } from "node:crypto";
+import type { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
+import { isInitializeRequest } from "@modelcontextprotocol/sdk/types.js";
+import { toFetchResponse, toReqRes } from "fetch-to-node";
+
+export const registerMcp = (api: Hono, getServer: () => McpServer) => {
+  const transports: { [sessionId: string]: StreamableHTTPServerTransport } = {};
+
+  const handleSessionRequest = async (c: Context) => {
+    const sessionId = c.req.header("Mcp-Session-Id")?.toString() ?? "";
+    if (!sessionId || !transports[sessionId]) {
+      return c.text("Invalid or missing session ID", 400);
+    }
+
+    const transport = transports[sessionId];
+    const { req, res } = toReqRes(c.req.raw);
+    await transport.handleRequest(req, res);
+    return toFetchResponse(res);
+  };
+
+  api
+    .post("/mcp", async (c) => {
+      const sessionId = c.req.header("Mcp-Session-Id")?.toString() ?? "";
+      const body = await c.req.json();
+      let transport = transports[sessionId];
+      if (!transport && isInitializeRequest(body)) {
+        transport = new StreamableHTTPServerTransport({
+          sessionIdGenerator: () => randomUUID(),
+          onsessioninitialized: (sessionId) => {
+            transports[sessionId] = transport!;
+          },
+        });
+        transport.onerror = console.error.bind(console);
+        transport.onclose = () => {
+          if (transport?.sessionId) {
+            delete transports[transport.sessionId];
+          }
+        };
+        await getServer().connect(transport);
+      }
+      if (!transport) {
+        return c.json(
+          {
+            jsonrpc: "2.0",
+            error: {
+              code: -32000,
+              message: "Bad Request: No valid session ID provided",
+            },
+            id: null,
+          },
+          400,
+        );
+      }
+
+      const { req, res } = toReqRes(c.req.raw);
+      await transport.handleRequest(req, res, body);
+      return toFetchResponse(res);
+    })
+    .get(handleSessionRequest)
+    .delete(handleSessionRequest);
+};
