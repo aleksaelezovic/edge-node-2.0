2a39090edfc26ee24e3cd76b89244d9280e41dca
aleksaelezovic <aleksaelezovic@gmail.com>
2025-06-23 14:45:48 +0200

    Add example private key (from DKG.js)

--- typescript/apps/mcp-server/src/index.ts
+++ typescript/apps/mcp-server/src/index.ts
@@ -28,6 +28,8 @@ const api = createPluginApi({
       port: "8900",
       blockchain: {
         name: "hardhat1:31337",
+        privateKey:
+          "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
       },
       maxNumberOfRetries: 300,
       frequency: 2,
