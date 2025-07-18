8b6cc0d6c3c4f6736ca5ddbce1fbde8df41f72b3
aleksaelezovic <aleksaelezovic@gmail.com>
2025-07-18 10:01:56 +0200

    Remove mock routes and redirect '/' to '/chat'

--- typescript/apps/example-edge-node/src/app/_layout.tsx
+++ typescript/apps/example-edge-node/src/app/_layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="login" options={{ headerShown: false }} />
         <Stack.Screen name="chat" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
