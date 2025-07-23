b430d5c72d9b40c65f5efda87fe33402888c10fb
aleksaelezovic <aleksaelezovic@gmail.com>
2025-07-23 23:47:50 +0200

    Fix useColorScheme on the web, now registers changing of color scheme

--- typescript/apps/example-edge-node/src/hooks/useColorScheme.web.ts
+++ typescript/apps/example-edge-node/src/hooks/useColorScheme.web.ts
@@ -1,20 +1,36 @@
 import { useEffect, useState } from "react";
-import { useColorScheme as useRNColorScheme } from "react-native";
+import {
+  ColorSchemeName,
+  useColorScheme as useRNColorScheme,
+} from "react-native";
 
 /**
  * To support static rendering, this value needs to be re-calculated on the client side for web
  */
 export function useColorScheme() {
   const [hasHydrated, setHasHydrated] = useState(false);
+  const rnColorScheme = useRNColorScheme();
+  const [prefColorScheme, setPrefColorScheme] =
+    useState<ColorSchemeName>(rnColorScheme);
 
   useEffect(() => {
     setHasHydrated(true);
+    const pref = window.matchMedia("(prefers-color-scheme: dark)");
+    const handlePrefChange = (e: MediaQueryListEvent) => {
+      if (e.matches) {
+        setPrefColorScheme("dark");
+      } else {
+        setPrefColorScheme("light");
+      }
+    };
+    pref.addEventListener("change", handlePrefChange);
+    return () => {
+      pref.removeEventListener("change", handlePrefChange);
+    };
   }, []);
 
-  const colorScheme = useRNColorScheme();
-
   if (hasHydrated) {
-    return colorScheme;
+    return prefColorScheme;
   }
 
   return "light";
